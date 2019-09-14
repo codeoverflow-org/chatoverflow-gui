9e7c6245c189e04011007832414a5b420901f756
sebinside <hi@sebinside.de>
2019-09-14 23:30:35 +0200

    Extracted common styling out of the better repl gui for easier reading and better reuse.

--- src/app/layouts/common-layout/common-layout.component.ts
+++ src/app/layouts/common-layout/common-layout.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-common-layout',
   templateUrl: './common-layout.component.html',
+  styleUrls: ['./common.scss']
 })
 export class CommonLayoutComponent { }
